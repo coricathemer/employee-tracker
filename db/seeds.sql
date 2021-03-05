f6451949563fb63808890751ead0c0da0c5f8e54
coricathemer <coricathemer@gmail.com>
2021-03-04 23:19:21 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (id, dept_name)
+VALUES
+(1, 'interns'),
+(2, 'engineers'),
+(3, 'sales')
+(4, 'human resources');
+
+INSERT INTO position (id, title, salary, department_id)
+VALUES
+()
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+()
